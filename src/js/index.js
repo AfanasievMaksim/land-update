59afdc9565b3fb5c864645be3f80c67c2af97c71
MaksymAfanasiev <afanasiev.maksym@pdffiller.team>
2021-09-22 01:32:16 +0300

    fixes

--- src/js/index.js
+++ src/js/index.js
@@ -119,7 +119,8 @@ $(document).ready(function () {
 
     if (
       $(".threesixty__dropdown").hasClass("active") &&
-      !$(e.target).hasClass("threesixty__dropdown-title")
+      !$(e.target).hasClass("threesixty__dropdown-title") &&
+      $(e.target).closest('.threesixty__dropdown').length === 0
     ) {
       $(".threesixty__dropdown").removeClass("active");
     }
@@ -1274,6 +1275,45 @@ $("#modal-photos").on("hidden.bs.modal", function (e) {
 //360
 import screenfull from 'screenfull'
 
+let imageData = $('#modal-360 .threesixty__dropdown-item.active').data('imageFolder');
+let imageDownloaded = 0;
+
+let dragging = false;
+let imageIndex = 0;
+let dragStart = 0;
+let rotation = 0;
+let prevRotation = 0;
+let rotate = 0;
+let pixelsPerDegree = 10;
+
+const loadingImageCB = () => {
+  imageDownloaded += 100 / 36;
+  if (imageDownloaded >= 99) {
+    $('.threesixty-container').addClass('loaded')
+    $('.js-loader-percent').text(`0%`)
+  } else {
+    $('.js-loader-percent').text(`${Math.round(imageDownloaded)}%`)
+  }
+}
+
+const renderImage = () => {
+  const imageContainer = $('.threesixty-images__container')
+  imageContainer.empty();
+  let classes = 'image'
+  for (let i = 1; i < 37; i++) {
+    if (i === 1) {
+      classes += ' active'
+    } else {
+      classes = 'image'
+    }
+    const image = new Image();
+    image.src = `${imageData}${i}.jpg`
+    image.onload = loadingImageCB
+    image.classList = classes
+    imageContainer.append( image );
+  }
+}
+
 $('#js-toogle-fullscreen').on('click', function() {
   if (screenfull.isEnabled) {
     screenfull.toggle(document.querySelector('.threesixty-container'));
@@ -1288,15 +1328,115 @@ screenfull.on('change', () => {
   }
 });
 
-$('.threesixty__dropdown').on('click', function() {
+$('.threesixty__dropdown').on('click', function(e) {
   $(this).addClass('active');
 })
 
-$('.threesixty__dropdown-item').on('click', function() {
+$('.threesixty__dropdown-item').on('click', function(e) {
+  e.stopPropagation();
   $('.threesixty__dropdown-item').removeClass('active');
+  $(".threesixty__dropdown").removeClass("active");
   $('.threesixty__dropdown-title').text($(this).text())
+  $('.threesixty-container').removeClass('loaded')
+  imageDownloaded = 0;
+  imageData = $(this).data('imageFolder');
+  imageIndex = 0;
+  dragStart = 0;
+  rotation = 0;
+  prevRotation = 0;
+  rotate = 0;
+  $('.image-dron-inner').css({
+    transform: ''
+  })
+  renderImage();
   $(this).addClass('active');
 })
+
+$('#modal-360').on('shown.bs.modal', function () {
+  renderImage()
+})
+
+const handleDown = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    let pixelDiff
+    if (e.type === 'touchstart') {
+      pixelDiff = e.touches[0].screenX
+    } else {
+      pixelDiff = e.screenX
+    }
+
+    dragging = true;
+    dragStart = pixelDiff;
+  }
+
+const handleUp = () => {
+  dragging = false;
+}
+
+const updateImageIndex = (currentPosition) => {
+  $('.threesixty-container').addClass('touched');
+  let dx = dragStart - currentPosition
+  let newIndex
+
+  if (dx < 0) {
+    newIndex = imageIndex - 1
+
+    if (newIndex < 0) {
+      newIndex = 35
+    }
+  } else if (dx > 0) {
+    newIndex = imageIndex + 1
+    if (newIndex > 35) {
+      newIndex = 0
+    }
+  }
+
+  imageIndex = newIndex;
+
+  $($('.threesixty-images__container img')[imageIndex]).addClass('active').siblings().removeClass('active');
+  dragStart = currentPosition;
+  rotation = newIndex;
+
+  if (rotation > prevRotation || (prevRotation === 35 && rotation === 0)) {
+    rotate += 1;
+  } else if (rotation < prevRotation || (prevRotation === 0 && rotation === 35)) {
+    rotate -= 1;
+  }
+
+  $('.image-dron-inner').css({
+    transform: `rotate(${rotate * 10}deg)`
+  })
+
+  prevRotation = rotation
+}
+
+const handleMove = (e) => {
+  let pixelDiff
+  if (e.type === 'touchmove') {
+    pixelDiff = e.touches[0].screenX
+  } else {
+    pixelDiff = e.screenX
+  }
+
+  if (dragging && dragStart !== 0) {
+    if (Math.abs(dragStart - pixelDiff) > pixelsPerDegree) {
+      updateImageIndex(Math.round(pixelDiff))
+    }
+  }
+}
+
+const preventDragHandler = (e) => {
+  e.preventDefault()
+}
+
+$('.threesixty-images__container').on('dragstart', preventDragHandler);
+$('.threesixty-images__container').on('mouseup', handleUp);
+$('.threesixty-images__container').on('touchstart', handleDown);
+$('.threesixty-images__container').on('mousemove', handleMove);
+$('.threesixty-images__container').on('touchmove', handleMove);
+$('.threesixty-images__container').on('mousedown', handleDown);
+$('.threesixty-images__container').on('touchend', handleUp);
 
 // 3d
 // const ifwin = document.querySelector('.VR-layout').contentWindow;
